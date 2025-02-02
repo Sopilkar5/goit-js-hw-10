39fcb67dde6ff7bef066a75ad89f4490c2b8cd7c
Serhiy Tatomyre <combat.ygar@gmail.com>
2025-02-02 16:52:17 +0200

    so exception

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -20,12 +20,12 @@ flatpickr(datetimePicker, {
         message: 'Please choose a date in the future',
         position: 'topRight',
       });
-        startButton.disabled = true;
-        startButton.style.backgroundColor = "#cfcfcf";
+      startButton.disabled = true;
+      startButton.style.backgroundColor = "#cfcfcf";
     } else {
-        startButton.disabled = false;
-        startButton.style.backgroundColor = " #4e75ff";
-        startButton.style.color = "#ffff"
+      startButton.disabled = false;
+      startButton.style.backgroundColor = " #4e75ff";
+      startButton.style.color = "#ffff";
     }
   },
 });
@@ -49,30 +49,39 @@ function convertMs(ms) {
 }
 
 function updateTimer() {
-  const timeLeft = userSelectedDate - new Date();
-  const { days, hours, minutes, seconds } = convertMs(timeLeft);
-
-  document.querySelector('[data-days]').textContent = addLeadingZero(days);
-  document.querySelector('[data-hours]').textContent = addLeadingZero(hours);
-  document.querySelector('[data-minutes]').textContent = addLeadingZero(minutes);
-  document.querySelector('[data-seconds]').textContent = addLeadingZero(seconds);
-р
+  const timeLeft = userSelectedDate - new Date(); 
+  
   if (timeLeft <= 0) {
-    clearInterval(countdownInterval);
+    clearInterval(countdownInterval); 
     startButton.disabled = false;
     datetimePicker.disabled = false;
+    startButton.style.backgroundColor = "#4e75ff"; 
     iziToast.success({
       title: 'Done',
       message: 'The timer has finished',
       position: 'topRight',
     });
+
+    document.querySelector('[data-days]').textContent = "00";
+    document.querySelector('[data-hours]').textContent = "00";
+    document.querySelector('[data-minutes]').textContent = "00";
+    document.querySelector('[data-seconds]').textContent = "00";
+
+    return; 
   }
+
+  const { days, hours, minutes, seconds } = convertMs(timeLeft);
+  
+  document.querySelector('[data-days]').textContent = addLeadingZero(days);
+  document.querySelector('[data-hours]').textContent = addLeadingZero(hours);
+  document.querySelector('[data-minutes]').textContent = addLeadingZero(minutes);
+  document.querySelector('[data-seconds]').textContent = addLeadingZero(seconds);
 }
 
 startButton.addEventListener('click', () => {
   startButton.disabled = true;
   datetimePicker.disabled = true;
-  startButton.style.backgroundColor = "";
-  countdownInterval = setInterval(updateTimer, 1000);
+  startButton.style.backgroundColor = ""; 
+  countdownInterval = setInterval(updateTimer, 1000); 
 });
 
